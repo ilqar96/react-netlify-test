090775f2af152fd9017fc3e2b310d607d6267cfe
İlqar <ilqar.huseynli.9686@gmail.com>
2021-03-08 19:48:35 +0400

    test counter

---
+++ src/layouts/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+function App() {
+
+    const [count, setCount] = useState(0);
+
+    const onClickButton = (actionName) => {
+        let countValue = count;
+        if (actionName === 'minus' && countValue > 0) {
+            setCount(--countValue)
+        } else if(actionName === 'plus') {
+            setCount(++countValue)
+        }
+    }
+
+    return (
+        <div className='container center'>
+            <h1>App test</h1>
+            <h3>Counter</h3>
+
+            <div className="row mt-3">
+                <button onClick={() => onClickButton('plus')}>+</button>
+                <p>{count}</p>
+                <button onClick={() => onClickButton('minus')}>-</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
